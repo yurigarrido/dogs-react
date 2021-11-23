2c64098b89e8740bd08e753bd0dbd435bbd8e3b5
yurigarrido <y.garrido.yuri@gmail.com>
2021-11-23 20:33:52 -0300

    adicona a rota pra pagina de login e pra home, alem de criar o componente do header e footer

---
+++ dogs/src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Footer.module.css'
+
+
+const Footer = () => {
+  return (
+    <div className={styles.Footer}>
+      footer
+    </div>
+  )
+}
+
+export default Footer
