9f5150f47ac416288706315079b7e8e56ffbe1aa
yurigarrido <y.garrido.yuri@gmail.com>
2021-11-25 17:41:58 -0300

    adicona provider global para funçoes de fetch e dados globais, modifica o header para quando esta logado

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
 
 function App() {
   return (
-    <div className="App">
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='login/*' element={<Login/>}/>
-      </Routes>
-      <Footer />
-     </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
     </div>
   );
 }
